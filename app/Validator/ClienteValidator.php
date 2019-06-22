7da20182ed5efe487c9ee835c9cbb1f6f6a564d6
araujocezar <cezar.cordeiro@dce.ufpb.brm>
2019-06-22 17:47:59 -0300

    Criação do backend do Cliente

---
+++ app/Validator/ClienteValidator.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Validator;
+use App\Cliente;
+class ClienteValidator{
+    public static function validate($dados){
+        $validator = \Validator::make($dados, Cliente::$rules, Cliente::$messages);
+        if(!$validator->errors()->isEmpty()){
+            //throw new ValidationException($validator, $validator->errors());
+        }
+    }
+}
---
+++ database/factories/EnderecoFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Endereco;
+use Faker\Generator as Faker;
+
+$factory->define(Endereco::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
