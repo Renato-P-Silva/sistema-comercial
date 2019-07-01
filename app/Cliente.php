01b695b44e29e157134cbd17bd2c44d0be4a02ac
victorsoouto <victorsoouto@gmail.com>
2019-07-01 15:57:38 -0300

    Teste de Cliente e Produto

--- app/Cliente.php
+++ app/Cliente.php
@@ -7,12 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Cliente extends Model
 {
 
-  public static $rules = [
-        'nome' => 'required',
-  ];
-  
+    //Rules configuradas diretamente no Validator para melhor validacao de campos unicos
+    
   public static $messages = [
       'required' => 'O campo :attribute é obrigatório',
+      'min' => 'O campo :attribute deve ter no minimo 2 digitos',
+      'max' => 'o campo :attribute deve ter no maximo 100 digitos',
+      'cpf.min' => 'O campo cpf deve ter exatamente 11 digitos',
+      'cpf.max' => 'O campo cpf deve ter exatamente 11 digitos',
+      'unique' => 'Este :attribute já existe',
+      'telefone.min' => 'O campo :attribute deve ter no minimo 7 digitos',
+      'telefone.max' => 'O campo :attribute deve ter no maximo 20 digitos',
+    
+
   ];
     public function endereco(){
         return $this->hasOne(\App\Endereco::class);
--- app/Validator/ClienteValidator.php
+++ app/Validator/ClienteValidator.php
@@ -3,9 +3,16 @@ namespace App\Validator;
 use App\Cliente;
 class ClienteValidator{
     public static function validate($dados){
-        $validator = \Validator::make($dados, Cliente::$rules, Cliente::$messages);
+        $validator = \Validator::make($dados, 
+        [
+        'nome' => 'required|min:2|max:100',
+        'cpf' => 'required|unique:clientes,cpf,'.$dados['id'].'|min:11|max:11',
+        'email' => 'required|unique:clientes,email,'.$dados['id'],
+        'telefone' => 'required|min:7|max:20',
+        ],
+         Cliente::$messages);
         if(!$validator->errors()->isEmpty()){
-            //throw new ValidationException($validator, $validator->errors());
+            throw new ValidationException($validator, $validator->errors());
         }
     }
 }
